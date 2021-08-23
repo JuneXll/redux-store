ec45e9983dfa361e4f6cf104f3481462c83fe09f
JuneXll <71652307+JuneXll@users.noreply.github.com>
2021-08-22 23:06:38 -0400

    Updated util files accordingly

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,23 +1,26 @@
-import React, { createContext, useContext } from "react";
-import { useProductReducer } from './reducers'
+import React from "react";
+import { Provider } from 'react-redux';
+import store from './store';
 
-const StoreContext = createContext();
-const { Provider } = StoreContext;
 
-const StoreProvider = ({ value = [], ...props }) => {
-  const [state, dispatch] = useProductReducer({
-    products: [],
-    cart: [],
-    cartOpen: false,
-    categories: [],
-    currentCategory: '',
-  });
+// const StoreProvider = ({ value = [], ...props }) => {
+//   const [state, dispatch] = useProductReducer({
+//     products: [],
+//     cart: [],
+//     cartOpen: false,
+//     categories: [],
+//     currentCategory: '',
+//   });
 
-  return <Provider value={[state, dispatch]} {...props} />;
+//   return <Provider value={[state, dispatch]} {...props} />;
+// };
+
+const StoreProvider = ({...props})=>{
+  return<Provider store={store} {...props}/>
 };
 
-const useStoreContext = () => {
-  return useContext(StoreContext);
-};
+// const useStoreContext = () => {
+//   return useContext(StoreContext);
+// };
 
-export { StoreProvider, useStoreContext };
+export { StoreProvider};
